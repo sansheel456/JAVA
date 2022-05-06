165294971ad036073ea2810b74ab03f6dcd2e20c
sansheel456 <104505018+sansheel456@users.noreply.github.com>
2022-05-06 10:55:07 +0530

    Add files via upload

---
+++ assignments/PrimitivedatatypesA2.java
@@ -0,0 +1,24 @@
+
+public class PrimitivedatatypesA2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a=21;
+		char b='s';
+		int c=1675;
+		double d=8765345D;
+		long e=1000000L;
+		float f=2367.766f;
+		short g=3;
+		boolean h=true;
+		System.out.println("Byte data= "+a);
+		System.out.println("Char data= "+b);
+		System.out.println("Int data= "+c);
+		System.out.println("Double data= "+d);
+		System.out.println("Long data= "+e);
+		System.out.println("Float data= "+f);
+		System.out.println("Short data= "+g);
+		System.out.println("Boolean data= "+h);
+	}
+
+}
